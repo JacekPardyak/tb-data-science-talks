49265d7f6012c22f06d2a4209b006d609c9831ff
unknown <jacek.pardyak@gmail.com>
2022-03-12 23:02:00 +0100

    audio

--- 2022-06-30/script.R
+++ 2022-06-30/script.R
@@ -1,89 +1,85 @@
-#jacek_pardyak #O-1
-#jacekpardyak
+# Geospatial data available at the geojson format
+library(geojsonio)
+spdf <- geojson_read("https://raw.githubusercontent.com/gregoiredavid/france-geojson/master/communes.geojson",  what = "sp")
 
-# person you want to send direct message has to follow you
-# your app has to set up permissions:
-# Read, Write and Access direct messages
+# Since it is a bit too much data, I select only a subset of it:
+spdf <- spdf[ substr(spdf@data$code,1,2)  %in% c("06", "83", "13", "30", "34", "11", "66") , ]
 
-library(twitteR)
+spdf
 
-dmSend(paste(Sys.time(), "test", sep = " "), "jacekpardyak")
+library(broom)
+spdf_fortified <- tidy(spdf, region = "code")
 
+# Now I can plot this shape easily as described before:
+library(ggplot2)
+ggplot() +
+  geom_polygon(data = spdf_fortified, aes( x = long, y = lat, group = group), fill="white", color="grey") +
+  theme_void() +
+  coord_map()
 
+# read data
+data <- read.table("https://raw.githubusercontent.com/holtzy/R-graph-gallery/master/DATA/data_on_french_states.csv", header=T, sep=";")
+head(data)
 
-msg <- function(){# function to send a message
-  user <- readline(prompt="Enter a username: ")
-  text <- readline(prompt="Enter a text: ")
-  dmSend(text, user)
-  cat("Message sent")}
+# Distribution of the number of restaurant?
+library(dplyr)
+data %>%
+  ggplot( aes(x=nb_equip)) +
+  geom_histogram(bins=20, fill='skyblue', color='#69b3a2') + scale_x_log10()
 
-?dmSend
+# Make the merge
+spdf_fortified = spdf_fortified %>%
+  left_join(. , data, by=c("id"="depcom"))
 
+# Note that if the number of restaurant is NA, it is in fact 0
+spdf_fortified$nb_equip[ is.na(spdf_fortified$nb_equip)] = 0.001
 
-dmsg <- function(){
-  dmDestroy(
-    receMessages[[1]]$getId()
-    )
-  }
+ggplot() +
+  geom_polygon(data = spdf_fortified, aes(fill = nb_equip, x = long, y = lat, group = group)) +
+  theme_void() +
+  coord_map()
 
-received <- dmGet()
-sent     <- dmSent()
-messages <- c(received, sent)
-messages <- lapply(messages, function(x){cbind(x$id,
-                                               x$senderSN,
-                                               x$recipientSN,
-                                               x$text)})
-messages <- data.frame(matrix(unlist(messages),
-                              nrow = length(messages), byrow=T)) 
-names(messages) <- c("id", "sender", "recipient", "text")
-
-messages <- messages[order(messages[,"id"]),]
-
-i = 2
-message <- messages[i,]
-
-cat(paste(paste(message[,2], "said:", sep = " "), message[,4], sep = " "))
-
-# delete a message
-
-dmDestroy(received[[1]])
-
-print("la")
-or(i in 1:length(messages)){
-message <- messages[[i]]
-row <- cbind(id = message$id,
-             sender = message$senderSN,
-             recipient = message$recipientSN,
-             text = message$text)
-res <- rbind(row,res)
-}
-
-
-
-yy <- data.frame(tt)
-
-
-
-00
-chat[[1]]$`.->text`
-chat[[1]]$text
-
-message$id
+library(viridis)
+p <- ggplot() +
+  geom_polygon(data = spdf_fortified, aes(fill = nb_equip, x = long, y = lat, group = group) , size=0, alpha=0.9) +
+  theme_void() +
+  scale_fill_viridis(trans = "log", breaks=c(1,5,10,20,50,100), name="Number of restaurant", guide = guide_legend( keyheight = unit(3, units = "mm"), keywidth=unit(12, units = "mm"), label.position = "bottom", title.position = 'top', nrow=1) ) +
+  labs(
+    title = "South of France Restaurant concentration",
+    subtitle = "Number of restaurant per city district",
+    caption = "Data: INSEE | Creation: Yan Holtz | r-graph-gallery.com"
+  ) +
+  theme(
+    text = element_text(color = "#22211d"),
+    plot.background = element_rect(fill = "#f5f5f2", color = NA),
+    panel.background = element_rect(fill = "#f5f5f2", color = NA),
+    legend.background = element_rect(fill = "#f5f5f2", color = NA),
+    
+    plot.title = element_text(size= 22, hjust=0.01, color = "#4e4d47", margin = margin(b = -0.1, t = 0.4, l = 2, unit = "cm")),
+    plot.subtitle = element_text(size= 17, hjust=0.01, color = "#4e4d47", margin = margin(b = -0.1, t = 0.43, l = 2, unit = "cm")),
+    plot.caption = element_text( size=12, color = "#4e4d47", margin = margin(b = 0.3, r=-99, unit = "cm") ),
+    
+    legend.position = c(0.7, 0.09)
+  ) +
+  coord_map()
+p
 
 
-toDataFrame(chat[[1]])
-  sender
-  recipient
-  text
-summary(chat)
+# fake animatin
+spdf_fortified_2017 <- spdf_fortified %>% mutate(year = 2017)
+spdf_fortified_2018 <- spdf_fortified %>% mutate(year = 2018) %>%
+  mutate(nb_equip = sample(nb_equip))
 
-chat$getSender()
+spdf_fortified_df <- spdf_fortified_2017 %>% 
+  bind_rows(spdf_fortified_2018)
 
-chat[[1]]
-class(chat)
-
-chat@text
-
-chat$toDataFrame()
-as.data.frame(chat)
-chat$t
+library(gganimate)
+ggplot() +
+  geom_polygon(data = spdf_fortified_df, 
+               aes(fill = nb_equip, x = long, y = lat, group = group)) +
+  theme_void() +
+  coord_map() +
+  labs(title = "Date: {frame_time}", size = "Users") +
+  transition_time(year) 
+  
+  
